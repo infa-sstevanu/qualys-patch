03c740561e59a0ed75cf82cbed5de8190c9df3e4
infa-sstevanu <sstevanus@informatica.com>
2021-01-07 11:01:14 -0800

    Update the patch_vm.py file

--- patch_vm.py
+++ patch_vm.py
@@ -5,19 +5,37 @@ import logging
 
 logging.basicConfig(format='[%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S%p]')
 
-parser = argparse.ArgumentParser()
+ignore_keywords = ["Package", "Installed", "Version", "Required", "Vulnerable", "Detected"]
 
 packages = []
+
+def is_valid_package_report(line):
+    for keyword in ignore_keywords:
+        if keyword in line:
+            return False
+    return True
 
 def list_package_to_update(detections, severity):
     for detection in detections:
         try:
             if int(detection['SEVERITY']) >= severity:
-                package = detection['RESULTS']
-                packages.append(package)
+                results = detection['RESULTS'].split('\n')
+		
+                for result in results:
+                    if is_valid_package_report(result):
+                        res = result.split('\t')
+                        if len(res) == 3:
+                            package = {
+                                "package_name": str(res[0]),
+                                "installed_version": str(res[1]),
+                                "required_version": str(res[2]),
+                            }                        
+                            packages.append(package)
+
         except Exception as e:
             logging.error(e)
-    
+            sys.exit(1)
+
 def patch_vm(qualys_json_file, severity):
     try:
         with open(qualys_json_file, 'r') as f:
@@ -33,11 +51,17 @@ def patch_vm(qualys_json_file, severity):
 
     except Exception as e:
         logging.error(e)
+        sys.exit(1)
 
+    print("=============================")
+    print(" List of vulnerable packages ")
+    print("=============================")
     for package in packages:
         print(package)
             
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
     parser.add_argument(
                 "-f", 
                 "--qualys_json_file", 
@@ -61,6 +85,6 @@ if __name__ == '__main__':
     
     if not qualys_json_file or not severity:
         print("Usage: ./patch_vm.py -f <QUALYS_JSON_FILE> -s <SEVERITY>")
-        sys.exit(0)
+        sys.exit(1)
 
     patch_vm(qualys_json_file, severity)
